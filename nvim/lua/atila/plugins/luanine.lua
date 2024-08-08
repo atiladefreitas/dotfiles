dea82a77cfc63d0044b3a98dcb3f6340d351acd9
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2024-08-08 08:59:13 -0300

    nvim cleanup

--- nvim/lua/atila/plugins/luanine.lua
+++ nvim/lua/atila/plugins/luanine.lua
@@ -1,9 +1,11 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  requires = "nvim-tree/nvim-web-devicons",
-  config = function() require("lualine").setup {
-    options = {
-      theme = "catppuccin"
-    }  } 
-end,
+	"nvim-lualine/lualine.nvim",
+	requires = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+			},
+		})
+	end,
 }
--- nvim/lua/atila/plugins/neo-tree.lua
+++ nvim/lua/atila/plugins/neo-tree.lua
@@ -7,11 +7,6 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	opts = {
-		sources = {
-			"filesystem",
-			"buffers", -- Disable buffers tab
-			"git_status", -- Disable git tab
-		},
 		filesystem = {
 			filtered_items = {
 				visible = true,
