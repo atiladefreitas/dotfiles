694a4d96bb4b88b24fb553a7a7bae4fd70158a8a
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2025-01-13 21:16:59 -0300

    minor adjustments

--- nvim/lua/atila/plugins/lazyclip.lua
+++ nvim/lua/atila/plugins/lazyclip.lua
@@ -1,11 +1,15 @@
 -- smart clipboard manager
 return {
 	"atiladefreitas/lazyclip",
-	-- dir = "~/Documents/lazyclip",
+	-- dir = "~/Documents/plugins/lazyclip",
 	event = "VeryLazy",
 	version = "*",
 	config = function()
-		require("lazyclip").setup()
+		require("lazyclip").setup({
+			window = {
+				width = 80,
+			},
+		})
 	end,
 	keys = {
 		{ "<leader>Cw", ":lua require('lazyclip').show_clipboard()<CR>", desc = "Open Clipboard Manager" },
