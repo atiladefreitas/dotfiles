9a8c1149f6525d5b9dd3aa21aec1b73fd1c6a10b
atiladefreitas <atila.jcdefreitas@protonmail.com>
2025-07-03 15:17:51 -0300

    backup

--- nvim/lua/atila/plugins/lsp/mason.lua
+++ nvim/lua/atila/plugins/lsp/mason.lua
@@ -1,37 +1,36 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		version = "^1.0.0",
-		config = true, -- Mason setup directly here
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		version = "^1.0.0",
-		dependencies = { "mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"html",
-					"tailwindcss",
-					"lua_ls",
-					"emmet_ls",
-					"biome",
-				},
-			})
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		version = "^1.0.0",
-		dependencies = { "mason.nvim" },
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"prettier",
-					"stylua",
-					"eslint_d",
-				},
-			})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    version = "^1.0.0",
+    config = true, -- Mason setup directly here
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    version = "^1.0.0",
+    dependencies = { "mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "html",
+          "tailwindcss",
+          "lua_ls",
+          "emmet_ls",
+          "biome",
+        },
+      })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    version = "^1.0.0",
+    dependencies = { "mason.nvim" },
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "prettier",
+          "eslint_d",
+        },
+      })
+    end,
+  },
 }
