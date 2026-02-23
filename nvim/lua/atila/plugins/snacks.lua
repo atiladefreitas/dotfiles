458fe7671769b93b2d2486dc4a9f67c1175d731f
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2026-02-23 13:46:11 -0300

    backup

--- nvim/lua/atila/plugins/snacks.lua
+++ nvim/lua/atila/plugins/snacks.lua
@@ -9,32 +9,64 @@ return {
 			enabled = true,
 			sections = {
 				{ section = "header" },
+				{
+					pane = 2,
+					section = "terminal",
+					cmd = "colorscript -e square",
+					height = 5,
+					padding = 1,
+				},
 				{ section = "keys", gap = 1, padding = 1 },
-				-- { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-				-- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-				-- {
-				-- 	pane = 2,
-				-- 	icon = " ",
-				-- 	title = "Git Status",
-				-- 	section = "terminal",
-				-- 	enabled = function()
-				-- 		return Snacks.git.get_root() ~= nil
-				-- 	end,
-				-- 	cmd = "hub status --short --branch --renames",
-				-- 	height = 5,
-				-- 	padding = 1,
-				-- 	ttl = 5 * 60,
-				-- 	indent = 3,
-				-- },
+				{
+					pane = 2,
+					icon = " ",
+					desc = "Browse Repo",
+					padding = 1,
+					key = "b",
+					action = function()
+						Snacks.gitbrowse()
+					end,
+				},
+				function()
+					local in_git = Snacks.git.get_root() ~= nil
+					local cmds = {
+						{
+							title = "Open Issues",
+							cmd = "gh issue list -L 3",
+							key = "i",
+							action = function()
+								vim.fn.jobstart("gh issue list --web", { detach = true })
+							end,
+							icon = " ",
+							height = 7,
+						},
+						{
+							icon = " ",
+							title = "Git Status",
+							cmd = "git --no-pager diff --stat -B -M -C",
+							height = 10,
+						},
+					}
+					return vim.tbl_map(function(cmd)
+						return vim.tbl_extend("force", {
+							pane = 2,
+							section = "terminal",
+							enabled = in_git,
+							padding = 1,
+							ttl = 5 * 60,
+							indent = 3,
+						}, cmd)
+					end, cmds)
+				end,
 				{ section = "startup" },
 			},
 			preset = {
 				keys = {
-					{ icon = " ", key = "f", desc = "Find File", action = ":Telescope find_files" },
-					{ icon = " ", key = "g", desc = "Find Text", action = ":Telescope live_grep" },
-					{ icon = " ", key = "r", desc = "Recent Files", action = ":Telescope oldfiles" },
+					{ icon = " ", key = "f", desc = "Find File", action = ":Telescope find_files" },
+					{ icon = "󱎸 ", key = "g", desc = "Find Text", action = ":Telescope live_grep" },
+					{ icon = " ", key = "r", desc = "Recent Files", action = ":Telescope oldfiles" },
 					{
-						icon = " ",
+						icon = " ",
 						key = "c",
 						desc = "Config",
 						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
@@ -46,7 +78,7 @@ return {
 						action = ":Lazy",
 						enabled = package.loaded.lazy ~= nil,
 					},
-					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+					{ icon = "󰗼 ", key = "q", desc = "Quit", action = ":qa" },
 				},
 				header = [[
 
